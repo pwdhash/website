46067f4570d44f2fedbadb4dc9ccf03f2ea05b37
Rajat Soni <rajjatsoni801@gmail.com>
2020-10-02 01:07:38 +0530

    Update domain-extractor.js

--- domain-extractor.js
+++ domain-extractor.js
@@ -1,6 +1,6 @@
 /*
 Copyright 2005 Collin Jackson
-
+An Amazing Project
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 
     * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
